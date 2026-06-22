de9951d96ef103221a5eb0c0477e88ec52e0a5f0
Raphael Sourty <raphael.sourty@gmail.com>
2026-06-22 12:48:16 +0200

    chore: bump version to 1.6.0

--- next-plaid-api/python-sdk/next_plaid_client/__init__.py
+++ next-plaid-api/python-sdk/next_plaid_client/__init__.py
@@ -25,7 +25,7 @@ from .models import (
     RerankResponse,
 )
 
-__version__ = "1.5.7"
+__version__ = "1.6.0"
 __all__ = [
     "NextPlaidClient",
     "AsyncNextPlaidClient",
--- next-plaid-onnx/python/src/colbert_export/cli.py
+++ next-plaid-onnx/python/src/colbert_export/cli.py
@@ -82,7 +82,7 @@ Supported models:
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s 1.5.7",
+        version="%(prog)s 1.6.0",
     )
 
     args = parser.parse_args()
